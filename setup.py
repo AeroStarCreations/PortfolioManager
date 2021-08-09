a2d399a7ee8353433c7604becff5b72e29d86594
Nathan Balli <AeroStarCreations1@gmail.com>
2021-08-09 16:30:34 -0700

    Create .gitignore

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 import setuptools
 
 setuptools.setup(
+    # Needed to silence warnings (and to be a worthwhile package)
     name='portfolio-manager',
-    version=0.1,
-    description='Package for balanced investing',
-    url='#',
+    url='https://github.com/AeroStarCreations/PortfolioManager',
     author='Nathan Balli',
-    install_requires=[],
-    author_email='',
+    author_email='AeroStarCreations1@gmail.com',
+    # Needed to actually package something
     packages=setuptools.find_packages(),
+    # Needed for dependencies
+    install_requires=[],
+    # *strongly* suggested for sharing
+    version=0.1,
+    # The license can be anything you like
+    license='MIT',
+    description='Package for balanced investing',
+    # You will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read()
+    # Other
     zip_safe=False
 )
