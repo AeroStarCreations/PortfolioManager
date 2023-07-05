baa1c0c1f02614d85b858e0067c203f8d7697805
Nathan Balli <AeroStarCreations1@gmail.com>
2023-07-04 18:23:37 -0700

    Trying to figure out how imports work yet again

---
+++ portfolio_manager/objects/__init__.py
@@ -0,0 +1,8 @@
+from .account_details import AccountDetails
+from .allocation_category import AllocationCategory
+from .allocation import Allocation
+from .asset import Asset
+from .portfolio import Portfolio
+
+def tired():
+    print('tired')
